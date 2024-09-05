9f8f06b3ce06b37872629043f459e6164901f836
João Pedro Rodrigues Vieira <jpedrovieira34@gmail.com>
2024-09-05 19:45:17 -0300

    Update DbConnect.js

--- src/DbConnect.js
+++ src/DbConnect.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient("mongodb+srv://alura:123@aluracluster.hzit8.mongodb.net/?retryWrites=true&w=majority&appName=AluraCluster");
+const client = new MongoClient("mongoclientUrl");
 
 let collectionDocuments;
 
